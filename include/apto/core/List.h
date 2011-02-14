04414057c7fc99a46e82727e925f98f558f0c858
David M. Bryson <david@programerror.com>
2011-02-14 12:54:43 -0500

    Add List class wrapper around SparseVectorList, converting sparse vector list into a storage policy for lists.

--- include/apto/core/List.h
+++ include/apto/core/List.h
@@ -1,8 +1,8 @@
 /*
- *  SparseVectorList.h
+ *  List.h
  *  Apto
  *
- *  Created by David on 9/10/10.
+ *  Created by David on 2/10/10.
  *  Copyright 2010-2011 David Michael Bryson. All rights reserved.
  *  http://programerror.com/software/apto
  *
@@ -28,37 +28,41 @@
  *
  */
 
-#ifndef AptoCoreSparseVectorList_h
-#define AptoCoreSparseVectorList_h
+#ifndef AptoCoreList_h
+#define AptoCoreList_h
 
 #include "apto/core/Array.h"
 #include "apto/core/EntryHandle.h"
 #include "apto/core/Iterator.h"
 
+#include <cassert>
+
 
 namespace Apto {
   
-#define SEGMENT_SIZE 16
-  
-  template <class T> class SparseVectorList
+
+  template <class T> class SparseVector
   {
   private:
+    static const int SEGMENT_SIZE = 16;
+    
     int m_size;
     int m_segs;
     
-    class SparseVectorListIterator;
-    class SparseVectorListHandle;
+    class SparseVectorIterator;
+    class SparseVectorHandle;
+    
     struct ListSegment
     {
-      SparseVectorList<T>* list;
+      SparseVector<T>* list;
       int used;
-      T* entries[SEGMENT_SIZE];
+      T entries[SEGMENT_SIZE];
       ListSegment* prev;
       ListSegment* next;
-      SparseVectorListHandle* handles[SEGMENT_SIZE];
+      SparseVectorHandle* handles[SEGMENT_SIZE];
       
       ListSegment() : used(0), next(NULL) { ; }
-      ListSegment(SparseVectorList<T>* in_list, T* value, ListSegment* in_next, ListSegment* in_prev = NULL)
+      ListSegment(SparseVector<T>* in_list, T* value, ListSegment* in_next, ListSegment* in_prev = NULL)
       : list(in_list), used(1), prev(in_prev), next(in_next)
       {
         entries[0] = value; handles[0] = NULL;
@@ -67,13 +71,14 @@ namespace Apto {
     
     ListSegment* m_head_seg;
     ListSegment* m_tail_seg;
-    tArray<SparseVectorListIterator*> m_its;
+    Array<SparseVectorIterator*> m_its;
     
     
-  public:
-    SparseVectorList() : m_size(0), m_segs(0), m_head_seg(NULL), m_tail_seg(NULL) { ; }
-    explicit SparseVectorList(const SparseVectorList& list);
-    ~SparseVectorList()
+  protected:
+    SparseVector() : m_size(0), m_segs(0), m_head_seg(NULL), m_tail_seg(NULL) { ; }
+    explicit SparseVector(const SparseVector& rhs);
+    
+    ~SparseVector()
     {
       ListSegment* next = m_head_seg;
       while (next) {
@@ -83,12 +88,13 @@ namespace Apto {
       }
     }
     
-    int GetSize() const { return m_size; }
+    inline int GetSize() const { return m_size; }
     
-    T* GetFirst() { return (m_head_seg && m_head_seg->used) ? m_head_seg->entries[m_head_seg->used - 1] : NULL; }
+    inline T& GetFirst() { return m_head_seg->entries[m_head_seg->used - 1]; }
+    inline const T& GetFirst() const { return m_head_seg->entries[m_head_seg->used - 1]; }
     
     
-    void Push(T* value, EntryHandle** handle = NULL)
+    void Push(const T& value, EntryHandle** handle = NULL)
     {
       if (handle) delete *handle;
       if (m_head_seg && m_head_seg->used < SEGMENT_SIZE) {
@@ -96,7 +102,7 @@ namespace Apto {
         m_head_seg->used++;
         m_head_seg->entries[idx] = value;
         if (handle) {
-          *handle = m_head_seg->handles[idx] = new SparseVectorListHandle(m_head_seg, value);
+          *handle = m_head_seg->handles[idx] = new SparseVectorHandle(m_head_seg, value);
         } else {
           m_head_seg->handles[idx] = NULL;
         }
@@ -108,13 +114,13 @@ namespace Apto {
         } else {
           m_tail_seg = m_head_seg;
         }
-        if (handle) *handle = m_head_seg->handles[0] = new SparseVectorListHandle(m_head_seg, value);
+        if (handle) *handle = m_head_seg->handles[0] = new SparseVectorHandle(m_head_seg, value);
       }
       
       m_size++;
     }
     
-    void PushRear(T* value, EntryHandle** handle = NULL)
+    void PushRear(const T& value, EntryHandle** handle = NULL)
     {
       if (handle) delete *handle;
       if (m_tail_seg && m_tail_seg->used < SEGMENT_SIZE) {
@@ -142,24 +148,27 @@ namespace Apto {
       m_size++;
     }
     
-    void Remove(const T* value)
+    bool Remove(const T& value)
     {
       ListSegment* cur = m_head_seg;
       while (cur) {
-        if (removeFromSeg(value, cur)) return;
+        if (removeFromSeg(value, cur)) return true;
         cur = cur->next;
       }
-      assert(false);
+      return false;
     }
     
-    Iterator<T>* Iterator() { return new SparseVectorListIterator(this); }
+    Iterator<T>* Iterator() { return new SparseVectorIterator(this); }
+    ConstIterator<T>* Iterator() const { return new SparseVectorConstIterator(this); }
     
-    int GetDataSize() const { return sizeof(T*) * m_size; }
+
+  public:
+    int GetDataSize() const { return sizeof(T) * m_size; }
     int GetMemSize() const { return sizeof(ListSegment) * m_segs; }
     
   private:
     
-    const T* removeFromSeg(const T* value, ListSegment* cur)
+    bool removeFromSeg(const T& value, ListSegment* cur)
     {
       for (int entry_idx = 0; entry_idx < cur->used; entry_idx++) {
         if (cur->entries[entry_idx] == value) {
@@ -232,15 +241,15 @@ namespace Apto {
           }
           
           m_size--;
-          return value;
+          return true;
         }
       }
-      return NULL;
+      return false;
     }
     
     
     
-    class SparseVectorListIterator : public Iterator<T>
+    class SparseVectorIterator : public Iterator<T>
     {
       friend class SparseVectorList<T>;
     private:
@@ -248,20 +257,20 @@ namespace Apto {
       ListSegment* m_cur;
       int m_pos;
       
-      SparseVectorListIterator(); // @not_implemented
-      SparseVectorListIterator(const SparseVectorListIterator&); // @not_implemented
-      SparseVectorListIterator& operator=(const SparseVectorListIterator&); // @not_implemented
+      SparseVectorIterator(); // @not_implemented
+      SparseVectorIterator(const SparseVectorListIterator&); // @not_implemented
+      SparseVectorIterator& operator=(const SparseVectorListIterator&); // @not_implemented
       
       
     public:
-      SparseVectorListIterator(SparseVectorList<T>* list)
+      SparseVectorIterator(SparseVectorList<T>* list)
       : m_list(list), m_cur(list->m_head_seg), m_pos((list->m_head_seg) ? list->m_head_seg->used : -1)
       {
         list->m_its.Push(this);
       }
-      ~SparseVectorListIterator()
+      ~SparseVectorIterator()
       {
-        tArray<SparseVectorListIterator*>& its = m_list->m_its;
+        Array<SparseVectorIterator*>& its = m_list->m_its;
         for (int i = 0; i < its.GetSize(); i++) {
           if (its[i] == this) {
             if (i != (its.GetSize() - 1)) its[i] = its[its.GetSize() - 1];
@@ -274,7 +283,7 @@ namespace Apto {
       T* Get() {
         if (m_cur && m_pos >= 0 && m_pos < m_cur->used) {
           assert(m_cur->entries[m_pos]);
-          return m_cur->entries[m_pos];
+          return &m_cur->entries[m_pos];
         }
         return NULL;
       }
@@ -283,34 +292,91 @@ namespace Apto {
         if (m_cur && m_pos > 0) {
           m_pos--;
           assert(m_cur->entries[m_pos]);
-          return m_cur->entries[m_pos];
+          return &m_cur->entries[m_pos];
         }
         if (m_cur && m_pos <= 0) {
           m_cur = m_cur->next;
           if (m_cur) {
             m_pos = m_cur->used - 1;
             assert(m_cur->entries[m_pos]);
-            return m_cur->entries[m_pos];
+            return &m_cur->entries[m_pos];
           }
         }
         return NULL;
       }
     };
-    
-    
-    class SparseVectorListHandle : public EntryHandle
+
+    class SparseVectorConstIterator : public Iterator<T>
     {
       friend class SparseVectorList<T>;
     private:
+      SparseVectorList<T>* m_list;
+      ListSegment* m_cur;
+      int m_pos;
+      
+      SparseVectorConstIterator(); // @not_implemented
+      SparseVectorConstIterator(const SparseVectorListConstIterator&); // @not_implemented
+      SparseVectorConstIterator& operator=(const SparseVectorListConstIterator&); // @not_implemented
+      
+      
+    public:
+      SparseVectorConstIterator(SparseVectorList<T>* list)
+      : m_list(list), m_cur(list->m_head_seg), m_pos((list->m_head_seg) ? list->m_head_seg->used : -1)
+      {
+        list->m_its.Push(this);
+      }
+      ~SparseVectorConstIterator()
+      {
+        Array<SparseVectorIterator*>& its = m_list->m_its;
+        for (int i = 0; i < its.GetSize(); i++) {
+          if (its[i] == this) {
+            if (i != (its.GetSize() - 1)) its[i] = its[its.GetSize() - 1];
+            its.Resize(its.GetSize() - 1);
+            break;
+          }
+        }
+      }
+      
+      const T* Get() {
+        if (m_cur && m_pos >= 0 && m_pos < m_cur->used) {
+          assert(m_cur->entries[m_pos]);
+          return &m_cur->entries[m_pos];
+        }
+        return NULL;
+      }
+      
+      const T* Next() {
+        if (m_cur && m_pos > 0) {
+          m_pos--;
+          assert(m_cur->entries[m_pos]);
+          return &m_cur->entries[m_pos];
+        }
+        if (m_cur && m_pos <= 0) {
+          m_cur = m_cur->next;
+          if (m_cur) {
+            m_pos = m_cur->used - 1;
+            assert(m_cur->entries[m_pos]);
+            return &m_cur->entries[m_pos];
+          }
+        }
+        return NULL;
+      }
+    };
+
+    
+    class SparseVectorHandle : public EntryHandle
+    {
+      friend class SparseVector<T>;
+    private:
       ListSegment* m_seg;
-      T* m_entry;
+      T m_entry;
       
-      SparseVectorListHandle(); // @not_implemented
-      SparseVectorListHandle(const SparseVectorListHandle&); // @not_implemented
-      SparseVectorListHandle& operator=(const SparseVectorListHandle&); // @not_implemented
+      SparseVectorHandle(); // @not_implemented
+      SparseVectorHandle(const SparseVectorHandle&); // @not_implemented
+      SparseVectorHandle& operator=(const SparseVectorHandle&); // @not_implemented
       
       
-      SparseVectorListHandle(ListSegment* seg, T* entry) : m_seg(seg), m_entry(entry) { ; }
+      SparseVectorHandle(ListSegment* seg, const T& entry) : m_seg(seg), m_entry(entry) { ; }
       
     public:
       bool IsValid() const { return (m_seg); }
@@ -323,7 +389,35 @@ namespace Apto {
     };
   };
   
-#undef SEGMENT_SIZE
+
+  
+  
+  
+  template <class T, template <class> class StoragePolicy = SparseVector>
+  class List : public StoragePolicy<T>
+  {
+  public:
+    List() { ; }
+    template <class T1, SP1>
+    explicit List(const List<T1, SP1>& rhs)
+    {
+      
+    }
+    ~List() { ; }
+    
+    inline int GetSize() const { return SP::GetSize(); }
+    
+    inline void Push(const T& value, EntryHandle** handle = NULL) { SP::Push(value, handle); }
+    inline void PushRear(const T& value, EntryHandle** handle = NULL) { SP::PushRear(value, handle); }
+    
+    inline T& GetFirst() { assert(GetSize()); return SP::GetFirst(); }
+    inline const T& GetFirst() const { assert(GetSize()); return SP::GetFirst(); }
+    
+    inline bool Remove(const T& value) { return SP::Remove(value); }
+    
+    inline Iterator<T>* Iterator() { return SP::Iterator(); }
+    inline ConstIterator<T>* Iterator() const { return SP::Iterator(); }
+  };
   
 };
 
