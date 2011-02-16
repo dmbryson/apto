e91a0d775d867e75578b17260e674b96dc9641e4
David M. Bryson <david@programerror.com>
2011-02-16 17:16:07 -0500

    Implement a couple of support functions for FishersExact.

--- src/stat/FishersExact.cc
+++ src/stat/FishersExact.cc
@@ -31,8 +31,98 @@
 #include "apto/stat/ContingencyTable.h"
 #include "apto/stat/Functions.h"
 
+#include "apto/core/Array.h"
+#include "apto/core/Pair.h"
+
+#include <cmath>
+#include <limits>
+
+
+static struct PastPathLength {
+  double value;
+  int observed;
+  
+  PastPathLength(double in_value) : value(in_value), observed(1) { ; }
+};
+
+static struct FExactNode
+{
+  double shortest_path;
+  double longest_path;
+  Apto::Array<PastPathLength, Apto::Smart> past_entries;  
+};
+
+static double cummulativeGamma(double q, double alpha, bool& fault);
+static double logGamma(double x, bool& fault);
 
 double Apto::Stat::FishersExact(const ContingencyTable& table)
 {
+  if (table.MarginalTotal() == 0.0) return std::numeric_limits<double>::quiet_NaN();  // All elements are 0
   
+  
+  return 0.0;
+}
+
+
+
+double cummulativeGamma(double q, double alpha, bool& fault)
+{
+  if (q <= 0.0 || alpha <= 0.0) {
+    fault = true;
+    return 0.0;
+  }
+  
+  double f = exp(alpha * log(q) - logGamma(alpha + 1.0, fault) - q); // no need to test logGamma fail as an error is impossible
+  if (f == 0.0) {
+    fault = true;
+    return 0.0;
+  }
+  
+  fault = false;
+  
+  double c = 1.0;
+  double ret_val = 1.0;
+  double a = alpha;
+
+  do {
+    a += 1.0;
+    c = c * q / a;
+    ret_val += c;
+  } while (c / ret_val > (1e-6));
+  ret_val *= f;
+  
+  return ret_val;
+}
+
+double logGamma(double x, bool& fault)
+{
+  const double a1 = .918938533204673;
+  const double a2 = 5.95238095238e-4;
+  const double a3 = 7.93650793651e-4;
+  const double a4 = .002777777777778;
+  const double a5 = .083333333333333;
+  
+  if (x < 0.0) {
+    fault = true;
+    return 0.0;
+  }
+  
+  fault = false;
+  
+  double f = 0.0;
+  
+  if (x < 7.0) {
+    f = x;
+    
+    x += 1.0;
+    while (x < 7.0) {
+      f *= x;
+      x += 1.0;
+    }
+
+    f = -log(f);
+  }
+  
+  double z = 1 / (x * x);
+  return f + (x - .5) * log(x) - x + a1 + (((-a2 * z + a3) * z - a4) * z + a5) / x;
 }
