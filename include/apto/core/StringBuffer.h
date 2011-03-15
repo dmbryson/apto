0f68fc911dce9e77e3d9d173914994f6cdbee38b
David M. Bryson <david@programerror.com>
2011-03-15 13:19:19 -0400

    Implement StringBuffer with unit tests.   Currently only mirrors BasicString functionality, but allows mutable access to the characters.

--- include/apto/core/StringBuffer.h
+++ include/apto/core/StringBuffer.h
@@ -35,78 +35,79 @@
 #include <string.h>
 
 #include "apto/core/RefCount.h"
-#include "apto/core/SmartPtr.h"
-#include "apto/core/TypeSelect.h"
 
 
 namespace Apto {
   
-  template <class ThreadingModel = SingleThreaded> class StringBuffer
+  // Declarations used to allow string buffer type conversion
+  // --------------------------------------------------------------------------------------------------------------
+  
+  template <class ThreadingModel> class BasicString;
+  
+  
+  // String Buffer
+  // --------------------------------------------------------------------------------------------------------------
+  
+  class StringBuffer
   {
-  protected:
-    class StringRep : public TypeSelect<ThreadingModel::UseThreadSafe, MTRefCountObject, RefCountObject>::Result
-    {
-    private:
-      int m_size;
-      char* m_data;
-      
-    public:
-      explicit StringRep(int size) : m_size(size), m_data(new char[size + 1])
-      {
-        assert(m_data);
-        m_data[0] = '\0';
-        m_data[size] = '\0';
-      }
-      StringRep(int size, const char* str) : m_size(size), m_data(new char[size + 1])
-      {
-        assert(m_data);
-        memcpy(str, m_data, m_size);
-        m_data[size] = '\0';
-      }
-      StringRep(const StringRep& rhs) : m_size(rhs.m_size), m_data(new char[m_size + 1])
-      {
-        assert(m_data);
-        memcpy(rhs.m_data, m_data, m_size);
-        m_data[m_size] = '\0';
-      }
-      
-      ~StringRep() { delete [] m_data; }
-      
-      inline int GetSize() const { return m_size; }
-      inline const char* GetRep() const { return m_data; }
-      
-      char operator[](int index) const { return m_data[index]; }
-      char& operator[](int index) { return m_data[index]; }
-    };
+  public:
+    typedef char ValueType;
+    class Iterator;
+    class ConstIterator;
     
-    SmartPtr<StringRep, HeapStorage, InternalRCObject> m_value;
+  protected:
+    class StringBufferRep;
+    class CharProxy;
+
+  protected:
+    static const int BUFFER_INCREASE_MINIMUM = 4;
+    static const int BUFFER_INCREASE_FACTOR = 2;
+    static const int BUFFER_SHRINK_TEST_FACTOR = 4;
+    
+    inline void copyOnWrite();
+
+  protected:    
+    StringBufferRep* m_value;
     
   public:
-    StringBuffer(const char* str = "") : m_value((str) ? new StringRep(strlen(str), str) : new StringRep) { assert(m_value); }
-    StringBuffer(int size, const char* str) : m_value(new StringRep(size, str)) { assert(m_value); }
-    explicit StringBuffer(int size) : m_value(new StringRep(size)) { assert(m_value); }
-    StringBuffer(const StringBuffer& rhs) : m_value(rhs.m_value) { ; }
-    template <class T1> StringBuffer(const StringBuffer<T1>& rhs) : m_value(new StringRep(rhs.GetSize(), rhs.GetData())) { assert(false); }
+    inline StringBuffer(const char* str = "")
+      : m_value((str) ? new StringBufferRep(strlen(str), str) : new StringBufferRep) { assert(m_value); }
+    inline StringBuffer(int size, const char* str) : m_value(new StringBufferRep(size, str)) { assert(m_value); }
+    inline explicit StringBuffer(int size) : m_value(new StringBufferRep(size)) { assert(m_value); }
+    inline StringBuffer(const StringBuffer& rhs) : m_value(NULL) { this->operator=(rhs); }
+    template <class R>
+    inline StringBuffer(const BasicString<R>& rhs) : m_value(rhs.GetSize(), rhs.GetData()) { ; }
     
-    StringBuffer() { ; }
+    inline ~StringBuffer() { m_value->RemoveReference(); }
     
+    
+    // Property Access
     inline int GetSize() const { return m_value->GetSize(); }
-    const char* GetData() const { return m_value->GetRep(); }
-    
+    inline const char* GetData() const { return m_value->GetRep(); }
+    inline const char* GetCString() const { return m_value->GetRep(); }
     inline operator const char*() const { return m_value->GetRep(); }
+    template <class R>
+    inline operator BasicString<R>() const { return BasicString<R>(m_value->GetSize(), m_value->GetRep()); }
     
-    inline StringBuffer& operator=(const StringBuffer& rhs) { m_value = rhs.m_value; return *this; }
-    template <class T1> inline StringBuffer& operator=(const StringBuffer<T1>& rhs)
+
+    // Assignment
+    inline StringBuffer& operator=(const StringBuffer& rhs)
     {
-      m_value = new StringRep(rhs.GetSize(), rhs.GetData());
-      assert(m_value);
+      if (&rhs == this) return *this;
+      if (m_value) m_value->RemoveReference();
+      if (rhs.m_value->IsExclusive()) m_value = new StringBufferRep(*rhs.m_value);
+      else {
+        m_value = rhs.m_value;
+        m_value->AddReference();
+      }
       return *this;
     }
     
     inline StringBuffer& operator=(const char* rhs)
     {
       assert(rhs);
-      m_value = new StringRep(strlen(rhs), rhs);
+      m_value->RemoveReference();
+      m_value = new StringBufferRep(strlen(rhs), rhs);
       assert(m_value);
       return *this;
     }
@@ -119,7 +120,7 @@ namespace Apto {
       int i;
       for (i = 0; i < GetSize() && str[i] != '\0' && (*this)[i] == str[i]; i++);
       
-      if (i == GetSize() && str[i] != '\0') return 0;
+      if (i == GetSize() && str[i] == '\0') return 0;
       if (i < GetSize() && str[i] < (*this)[i]) return 1;
       return -1;
     }
@@ -131,14 +132,169 @@ namespace Apto {
       return true;
     }
     inline bool operator==(const char* rhs) const { return Compare(rhs) == 0; }
+    
     inline bool operator!=(const StringBuffer& rhs) const { return !operator==(rhs); }
     inline bool operator!=(const char* rhs) const { return Compare(rhs) != 0; }
+    
+    inline bool operator<(const StringBuffer& rhs) const { return Compare(rhs) < 0; }
     inline bool operator<(const char* rhs) const { return Compare(rhs) < 0; }
+    
+    inline bool operator>(const StringBuffer& rhs) const { return Compare(rhs) > 0; }
     inline bool operator>(const char* rhs) const { return Compare(rhs) > 0; }
+    
+    inline bool operator<=(const StringBuffer& rhs) const { return Compare(rhs) <= 0; }
     inline bool operator<=(const char* rhs) const { return Compare(rhs) <= 0; }
+    
+    inline bool operator>=(const StringBuffer& rhs) const { return Compare(rhs) >= 0; }
     inline bool operator>=(const char* rhs) const { return Compare(rhs) >= 0; }
+
+    
+    // Character access
+    inline char operator[](int index) const { return m_value->operator[](index); }
+    CharProxy operator[](int index) { return CharProxy(*this, index); }
+    
+    
+    // Concatenation
+    inline StringBuffer& operator+=(const char c) { return append(1, &c); }
+    inline StringBuffer& operator+=(const char* str) { return append(strlen(str), str); }
+    inline StringBuffer& operator+=(const StringBuffer& str) { return append(str.GetSize(), str.GetData()); }
+    inline StringBuffer operator+(const char c) { return concat(1, &c); }
+    inline StringBuffer operator+(const char* str) { return concat(strlen(str), str); }
+    inline StringBuffer operator+(const StringBuffer& str) { return concat(str.GetSize(), str.GetData()); }
+    
+    
+    // Substring extraction
+    inline StringBuffer Substring(int idx = 0, int length = -1)
+    {
+      assert(idx >= 0);
+      assert(idx < GetSize());
+      assert(length <= (GetSize() - idx));
+      return StringBuffer((length >= 0) ? length : (GetSize() - idx), m_value->GetRep() + idx);
+    }
+    
+    
+    // Various Character Inspection Utility Methods
+    inline bool IsLetter(int idx) const { return IsUpper(idx) || IsLower(idx); }
+    inline bool IsLower(int idx) const { return ((*this)[idx] >= 'a' && (*this)[idx] <= 'z'); }
+    inline bool IsUpper(int idx) const { return ((*this)[idx] >= 'A' && (*this)[idx] <= 'Z'); }
+    inline bool IsNumber(int idx) const { return (*this)[idx] >= '0' && (*this)[idx] <= '9'; }    
+    inline bool IsWhitespace(int idx) const
+    {
+      return (*this)[idx] == ' ' ||   // space
+      (*this)[idx] == '\f' ||  // form feed
+      (*this)[idx] == '\n' ||  // newline
+      (*this)[idx] == '\r' ||  // carriage return
+      (*this)[idx] == '\t' ||  // horizontal tab
+      (*this)[idx] == '\v';    // vertical tab
+    }
+    
+    
+    // Iterators
+    Iterator Begin() { return Iterator(m_value); }
+    ConstIterator Begin() const { return ConstIterator(m_value); }
     
   protected:
+    // Internal Support Methods
+    StringBuffer& append(int size, const char* str)
+    {
+      assert(size >= 0);
+      if (size == 0) return *this;
+      
+      assert(str != NULL);
+      copyOnWrite();
+      int old_size = m_value->GetSize();
+      m_value->Resize(old_size + size);
+      for (int i = 0; i < size; i++) m_value->operator[](i + old_size) = str[i];
+      return *this;
+    }
+    
+    StringBuffer concat(int size, const char* str)
+    {
+      assert(size >= 0);
+      if (size == 0) return StringBuffer(*this);
+      
+      StringBuffer rval(*this);
+      rval.append(size, str);
+      return rval;
+    }
+
+    
+    class StringBufferRep : public RefCountObject
+    {
+    private:
+      int m_capacity;
+      int m_size;
+      char* m_data;
+      
+    public:
+      explicit StringBufferRep(int size) : m_capacity(size + 1), m_size(size), m_data(new char[size + 1])
+      {
+        assert(m_data);
+        m_data[0] = '\0';
+        m_data[size] = '\0';
+      }
+      StringBufferRep(int size, const char* str) : m_capacity(size + 1), m_size(size), m_data(new char[size + 1])
+      {
+        assert(m_data);
+        memcpy(m_data, str, m_size);
+        m_data[size] = '\0';
+      }
+      StringBufferRep(const StringBufferRep& rhs) : m_capacity(rhs.m_size + 1), m_size(rhs.m_size), m_data(new char[m_size + 1])
+      {
+        assert(m_data);
+        memcpy(m_data, rhs.m_data, m_size);
+        m_data[m_size] = '\0';
+      }
+      
+      StringBufferRep() { delete [] m_data; }
+      
+      inline int GetSize() const { return m_size; }
+      inline const char* GetRep() const { return m_data; }
+      
+      char& operator[](int index) { return m_data[index]; }
+      char operator[](int index) const { return m_data[index]; }
+
+      void Resize(int new_size)
+      {
+        // If we're already at the size we want, don't bother doing anything.
+        if (new_size == m_size) return;
+        
+        // If new size is 0, clean up and go!
+        if (new_size == 0) {
+          if (m_data != NULL) delete [] m_data;
+          m_data = NULL;
+          m_size = 0;
+          m_capacity = 0;
+          return;
+        }
+        
+        // Determine if we need to adjust the allocated array sizes...
+        int new_capacity = new_size + 1;
+        int shrink_test = new_capacity * BUFFER_SHRINK_TEST_FACTOR;
+        if (new_capacity > m_capacity || shrink_test < m_capacity) {
+          new_capacity *= BUFFER_INCREASE_FACTOR;
+          int new_capacity_min = new_size + BUFFER_INCREASE_MINIMUM + 1;
+          if (new_capacity_min > new_capacity) new_capacity = new_capacity_min;
+          
+          char* new_data = new char[new_capacity];
+          assert(new_data != NULL); // Memory Allocation Error: Out of Memory?
+          
+          // Copy over old data...
+          for (int i = 0; i < m_size && i < new_size; i++) {
+            new_data[i] = m_data[i];
+          }
+          if (m_data != NULL) delete [] m_data;  // remove old data if exists
+          m_data = new_data;
+          
+          m_capacity = new_capacity;
+        }
+        
+        m_size = new_size;
+        m_data[new_size + 1] = '\0';  // NULL terminate character buffer
+      }
+    };
+    
+    
     class CharProxy
     {
     private:
@@ -148,24 +304,85 @@ namespace Apto {
     public:
       CharProxy(StringBuffer& str, int index) : m_str(str), m_index(index) { ; }
       
-      inline CharProxy& operator=(char rhs) { m_str.copyOnWrite(); m_str.m_value->operator[](index) = rhs; return *this; }
-      inline CharProxy& operator+=(char rhs) { m_str.copyOnWrite(); m_str.m_value->operator[](index) += rhs; return *this; }
-      inline CharProxy& operator-=(char rhs) { m_str.copyOnWrite(); m_str.m_value->operator[](index) -= rhs; return *this; }
-      inline CharProxy& operator++() { m_str.copyOnWrite(); ++m_str.m_value->operator[](index); return *this; }
-      inline char operator++(int) { m_str.copyOnWrite(); return m_str.m_value->operator[](index)++; }
-      inline CharProxy& operator--() { m_str.copyOnWrite(); --m_str.m_value->operator[](index); return *this; }
-      inline char operator--(int) { m_str.copyOnWrite(); return m_str.m_value->operator[](index)--; }
-      inline operator char() const { return m_str.m_value->operator[](index); }
+      inline CharProxy& operator=(char rhs) { m_str.copyOnWrite(); m_str.m_value->operator[](m_index) = rhs; return *this; }
+      inline CharProxy& operator+=(char rhs) { m_str.copyOnWrite(); m_str.m_value->operator[](m_index) += rhs; return *this; }
+      inline CharProxy& operator-=(char rhs) { m_str.copyOnWrite(); m_str.m_value->operator[](m_index) -= rhs; return *this; }
+      inline CharProxy& operator++() { m_str.copyOnWrite(); ++m_str.m_value->operator[](m_index); return *this; }
+      inline char operator++(int) { m_str.copyOnWrite(); return m_str.m_value->operator[](m_index)++; }
+      inline CharProxy& operator--() { m_str.copyOnWrite(); --m_str.m_value->operator[](m_index); return *this; }
+      inline char operator--(int) { m_str.copyOnWrite(); return m_str.m_value->operator[](m_index)--; }
+      inline operator char() const { return m_str.m_value->operator[](m_index); }
     };
     friend class CharProxy;
     
-    inline void copyOnWrite() { m_value = new StringRep(m_value); }
     
   public:
-    inline char operator[](int index) const { return m_value->operator[](index); }
-    CharProxy operator[](int index) { return CharProxy(*this, index); }
+    class Iterator
+    {
+      friend class StringBuffer;
+      
+    private:
+      StringBufferRep* m_value;
+      int m_index;
+      
+      inline Iterator(StringBufferRep* value) : m_value(value), m_index(-1) { m_value->AddReference(); }
+      
+    public:
+      inline ~Iterator() { m_value->RemoveReference(); }
+      inline const char* Get()
+      {
+        return (m_index < 0 || m_index >= m_value->GetSize()) ? NULL : &m_value->operator[](m_index);
+      }
+      
+      inline const char* Next()
+      {
+        if (m_index == m_value->GetSize()) return NULL;
+        return (++m_index == m_value->GetSize()) ? NULL : &m_value->operator[](m_index);
+      }
+    };
     
+    class ConstIterator
+    {
+      friend class StringBuffer;
+      
+    private:
+      StringBufferRep* m_value;
+      int m_index;
+      
+      inline ConstIterator(StringBufferRep* value) : m_value(value), m_index(-1) { m_value->AddReference(); }
+      
+    public:
+      inline ~ConstIterator() { m_value->RemoveReference(); }
+      inline const char* Get()
+      {
+        return (m_index < 0 || m_index >= m_value->GetSize()) ? NULL : &m_value->operator[](m_index);
+      }
+      
+      inline const char* Next()
+      {
+        if (m_index == m_value->GetSize()) return NULL;
+        return (++m_index == m_value->GetSize()) ? NULL : &m_value->operator[](m_index);
+      }
+    };
   };
+  
+  inline bool operator==(const char* lhs, const StringBuffer& rhs) { return rhs == lhs; }
+  inline bool operator!=(const char* lhs, const StringBuffer& rhs) { return rhs != lhs; }
+  
+  inline bool operator<(const char* lhs, const StringBuffer& rhs) { return rhs > lhs; }
+  inline bool operator>(const char* lhs, const StringBuffer& rhs) { return rhs < lhs; }
+  
+  inline bool operator<=(const char* lhs, const StringBuffer& rhs) { return rhs >= lhs; }
+  inline bool operator>=(const char* lhs, const StringBuffer& rhs) { return rhs <= lhs; }
+  
+  inline void StringBuffer::copyOnWrite()
+  {
+    if (!m_value->IsExclusive()) {
+      StringBufferRep* old_value = m_value;
+      m_value = new StringBufferRep(*old_value);
+      old_value->RemoveReference();
+    }
+  }
 };  
 
 #endif
--- unittests/core/StringBuffer.cc
+++ unittests/core/StringBuffer.cc
@@ -28,6 +28,289 @@
  *
  */
 
+//#include "apto/core/String.h"
 #include "apto/core/StringBuffer.h"
 
 #include "gtest/gtest.h"
+
+
+
+TEST(CoreStringBuffer, Construction) {
+  Apto::StringBuffer s1;
+  EXPECT_EQ(0, s1.GetSize());
+  
+  // Test Construction with strings
+  const char* teststr = "Foo Bar Baz";
+  Apto::StringBuffer s2(teststr);
+  EXPECT_EQ(11, s2.GetSize());
+  Apto::StringBuffer s3(3, teststr);
+  EXPECT_EQ(3, s3.GetSize());
+}
+
+
+TEST(CoreStringBuffer, Indexing) {
+  Apto::StringBuffer s1("foo");
+  EXPECT_EQ('f', s1[0]);
+  EXPECT_EQ('o', s1[1]);
+  EXPECT_EQ('o', s1[2]);
+  
+  s1[0] = 'b';
+  s1[1] = 'a';
+  s1[2] = 'r';
+  
+  EXPECT_EQ('b', s1[0]);
+  EXPECT_EQ('a', s1[1]);
+  EXPECT_EQ('r', s1[2]);
+  
+  // Make a copy (shared backing buffer)
+  Apto::StringBuffer s2(s1);
+  s2[2] = 'z';
+  
+  EXPECT_EQ('b', s1[0]);
+  EXPECT_EQ('a', s1[1]);
+  EXPECT_EQ('r', s1[2]);
+  
+  EXPECT_EQ('b', s2[0]);
+  EXPECT_EQ('a', s2[1]);
+  EXPECT_EQ('z', s2[2]);
+}
+
+
+TEST(CoreStringBuffer, Assignment) {
+  Apto::StringBuffer s1("foo");
+  EXPECT_EQ('f', s1[0]);
+  EXPECT_EQ('o', s1[1]);
+  EXPECT_EQ('o', s1[2]);
+  Apto::StringBuffer s2("bar");
+  EXPECT_EQ('b', s2[0]);
+  EXPECT_EQ('a', s2[1]);
+  EXPECT_EQ('r', s2[2]);
+  
+  Apto::StringBuffer s3(s1);
+  EXPECT_EQ('f', s3[0]);
+  EXPECT_EQ('o', s3[1]);
+  EXPECT_EQ('o', s3[2]);
+  
+  s2 = s1;
+  EXPECT_EQ('f', s2[0]);
+  EXPECT_EQ('o', s2[1]);
+  EXPECT_EQ('o', s2[2]);
+  
+  s3 = "baz";
+  EXPECT_EQ('b', s3[0]);
+  EXPECT_EQ('a', s3[1]);
+  EXPECT_EQ('z', s3[2]);
+}
+
+
+TEST(CoreStringBuffer, Iterators) {
+  Apto::StringBuffer s1("abcdef");
+  
+  Apto::StringBuffer::Iterator it = s1.Begin();
+  for (int i = 0; it.Next(); i++) EXPECT_EQ('a' + i, *it.Get());
+  
+  const Apto::StringBuffer& cs1 = s1;
+  Apto::StringBuffer::ConstIterator cit = cs1.Begin();
+  for (int i = 0; cit.Next(); i++) EXPECT_EQ('a' + i, *cit.Get());  
+}
+
+
+TEST(CoreStringBuffer, Comparison) {
+  Apto::StringBuffer s1("bar");
+  Apto::StringBuffer s2("baz");
+  
+  EXPECT_TRUE(s1 == s1);
+  EXPECT_TRUE(s2 == s2);
+  EXPECT_FALSE(s1 == s2);
+  EXPECT_FALSE(s2 == s1);
+  EXPECT_TRUE(s1 == "bar");
+  EXPECT_TRUE("bar" == s1);
+  EXPECT_FALSE(s1 == "baz");
+  EXPECT_FALSE("baz" == s1);
+  EXPECT_TRUE(s2 == "baz");
+  EXPECT_TRUE("baz" == s2);
+  EXPECT_FALSE(s2 == "bar");
+  EXPECT_FALSE("bar" == s2);
+  
+  EXPECT_FALSE(s1 != s1);
+  EXPECT_FALSE(s2 != s2);
+  EXPECT_TRUE(s1 != s2);
+  EXPECT_TRUE(s2 != s1);
+  EXPECT_FALSE(s1 != "bar");
+  EXPECT_FALSE("bar" != s1);
+  EXPECT_TRUE(s1 != "baz");
+  EXPECT_TRUE("baz" != s1);
+  EXPECT_FALSE(s2 != "baz");
+  EXPECT_FALSE("baz" != s2);
+  EXPECT_TRUE(s2 != "bar");
+  EXPECT_TRUE("bar" != s2);
+  
+  EXPECT_FALSE(s1 < s1);
+  EXPECT_FALSE(s2 < s2);
+  EXPECT_TRUE(s1 < s2);
+  EXPECT_FALSE(s2 < s1);
+  EXPECT_FALSE(s1 < "bar");
+  EXPECT_FALSE("bar" < s1);
+  EXPECT_TRUE(s1 < "baz");
+  EXPECT_FALSE("baz" < s1);
+  EXPECT_FALSE(s2 < "bar");
+  EXPECT_TRUE("bar" < s2);
+  EXPECT_FALSE(s2 < "baz");
+  EXPECT_FALSE("baz" < s2);
+  
+  EXPECT_FALSE(s1 > s1);
+  EXPECT_FALSE(s2 > s2);
+  EXPECT_FALSE(s1 > s2);
+  EXPECT_TRUE(s2 > s1);
+  EXPECT_FALSE(s1 > "bar");
+  EXPECT_FALSE("bar" > s1);
+  EXPECT_FALSE(s1 > "baz");
+  EXPECT_TRUE("baz" > s1);
+  EXPECT_TRUE(s2 > "bar");
+  EXPECT_FALSE("bar" > s2);
+  EXPECT_FALSE(s2 > "baz");
+  EXPECT_FALSE("baz" > s2);
+  
+  EXPECT_TRUE(s1 <= s1);
+  EXPECT_TRUE(s2 <= s2);
+  EXPECT_TRUE(s1 <= s2);
+  EXPECT_FALSE(s2 <= s1);
+  EXPECT_TRUE(s1 <= "bar");
+  EXPECT_TRUE("bar" <= s1);
+  EXPECT_TRUE(s1 <= "baz");
+  EXPECT_FALSE("baz" <= s1);
+  EXPECT_FALSE(s2 <= "bar");
+  EXPECT_TRUE("bar" <= s2);
+  EXPECT_TRUE(s2 <= "baz");
+  EXPECT_TRUE("baz" <= s2);
+  
+  EXPECT_TRUE(s1 >= s1);
+  EXPECT_TRUE(s2 >= s2);
+  EXPECT_FALSE(s1 >= s2);
+  EXPECT_TRUE(s2 >= s1);
+  EXPECT_TRUE(s1 >= "bar");
+  EXPECT_TRUE("bar" >= s1);
+  EXPECT_FALSE(s1 >= "baz");
+  EXPECT_TRUE("baz" >= s1);
+  EXPECT_TRUE(s2 >= "bar");
+  EXPECT_FALSE("bar" >= s2);
+  EXPECT_TRUE(s2 >= "baz");
+  EXPECT_TRUE("baz" >= s2);
+}
+
+
+TEST(CoreStringBuffer, Concatenation) {
+  Apto::StringBuffer s1("foo");
+  EXPECT_EQ(3, s1.GetSize());
+  EXPECT_EQ('f', s1[0]);
+  EXPECT_EQ('o', s1[1]);
+  EXPECT_EQ('o', s1[2]);
+  
+  s1 += 'b';
+  EXPECT_EQ(4, s1.GetSize());
+  EXPECT_EQ('f', s1[0]);
+  EXPECT_EQ('o', s1[1]);
+  EXPECT_EQ('o', s1[2]);
+  EXPECT_EQ('b', s1[3]);
+  
+  s1 += "ar";
+  EXPECT_EQ(6, s1.GetSize());
+  EXPECT_EQ('f', s1[0]);
+  EXPECT_EQ('o', s1[1]);
+  EXPECT_EQ('o', s1[2]);
+  EXPECT_EQ('b', s1[3]);
+  EXPECT_EQ('a', s1[4]);
+  EXPECT_EQ('r', s1[5]);
+  
+  s1 += Apto::StringBuffer("baz");
+  EXPECT_EQ(9, s1.GetSize());
+  EXPECT_EQ('f', s1[0]);
+  EXPECT_EQ('o', s1[1]);
+  EXPECT_EQ('o', s1[2]);
+  EXPECT_EQ('b', s1[3]);
+  EXPECT_EQ('a', s1[4]);
+  EXPECT_EQ('r', s1[5]);
+  EXPECT_EQ('b', s1[6]);
+  EXPECT_EQ('a', s1[7]);
+  EXPECT_EQ('z', s1[8]);
+  
+  s1 = s1 + ' ';
+  EXPECT_EQ(10, s1.GetSize());
+  EXPECT_EQ('f', s1[0]);
+  EXPECT_EQ('o', s1[1]);
+  EXPECT_EQ('o', s1[2]);
+  EXPECT_EQ('b', s1[3]);
+  EXPECT_EQ('a', s1[4]);
+  EXPECT_EQ('r', s1[5]);
+  EXPECT_EQ('b', s1[6]);
+  EXPECT_EQ('a', s1[7]);
+  EXPECT_EQ('z', s1[8]);
+  EXPECT_EQ(' ', s1[9]);
+  
+  
+  s1 = s1 + "- ";
+  EXPECT_EQ(12, s1.GetSize());
+  EXPECT_EQ('f', s1[0]);
+  EXPECT_EQ('o', s1[1]);
+  EXPECT_EQ('o', s1[2]);
+  EXPECT_EQ('b', s1[3]);
+  EXPECT_EQ('a', s1[4]);
+  EXPECT_EQ('r', s1[5]);
+  EXPECT_EQ('b', s1[6]);
+  EXPECT_EQ('a', s1[7]);
+  EXPECT_EQ('z', s1[8]);
+  EXPECT_EQ(' ', s1[9]);
+  EXPECT_EQ('-', s1[10]);
+  EXPECT_EQ(' ', s1[11]);
+  
+  
+  
+  s1 = s1 + s1;
+  EXPECT_EQ(24, s1.GetSize());
+  EXPECT_EQ('f', s1[0]);
+  EXPECT_EQ('o', s1[1]);
+  EXPECT_EQ('o', s1[2]);
+  EXPECT_EQ('b', s1[3]);
+  EXPECT_EQ('a', s1[4]);
+  EXPECT_EQ('r', s1[5]);
+  EXPECT_EQ('b', s1[6]);
+  EXPECT_EQ('a', s1[7]);
+  EXPECT_EQ('z', s1[8]);
+  EXPECT_EQ(' ', s1[9]);
+  EXPECT_EQ('-', s1[10]);
+  EXPECT_EQ(' ', s1[11]);
+  EXPECT_EQ('f', s1[12]);
+  EXPECT_EQ('o', s1[13]);
+  EXPECT_EQ('o', s1[14]);
+  EXPECT_EQ('b', s1[15]);
+  EXPECT_EQ('a', s1[16]);
+  EXPECT_EQ('r', s1[17]);
+  EXPECT_EQ('b', s1[18]);
+  EXPECT_EQ('a', s1[19]);
+  EXPECT_EQ('z', s1[20]);
+  EXPECT_EQ(' ', s1[21]);
+  EXPECT_EQ('-', s1[22]);
+  EXPECT_EQ(' ', s1[23]);
+}
+
+
+TEST(CoreStringBuffer, Substring) {
+  Apto::StringBuffer s1("foobarbaz");
+  Apto::StringBuffer s2 = s1.Substring(3, 3);
+  EXPECT_EQ(3, s2.GetSize());
+  EXPECT_EQ('b', s2[0]);
+  EXPECT_EQ('a', s2[1]);
+  EXPECT_EQ('r', s2[2]);
+}
+
+
+TEST(CoreStringBuffer, CharacterInstpection) {
+  Apto::StringBuffer s1("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789 \f\n\r\t\v.");
+  for (int i = 0; i < s1.GetSize(); i++) {
+    if (i <  26          ) EXPECT_TRUE(s1.IsUpper(i));  else EXPECT_FALSE(s1.IsUpper(i));
+    if (i >= 26 && i < 52) EXPECT_TRUE(s1.IsLower(i));  else EXPECT_FALSE(s1.IsLower(i));
+    if (i <  52          ) EXPECT_TRUE(s1.IsLetter(i)); else EXPECT_FALSE(s1.IsLetter(i));
+    if (i >= 52 && i < 62) EXPECT_TRUE(s1.IsNumber(i)); else EXPECT_FALSE(s1.IsNumber(i));
+    if (i >= 62 && i < 68) EXPECT_TRUE(s1.IsWhitespace(i)); else EXPECT_FALSE(s1.IsWhitespace(i));
+  }
+}
